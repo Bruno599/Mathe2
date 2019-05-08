4416561e7fac72e40a253ed6ce6afe5fcfae8b5d
Dominik <bruno481995@gmail.com>
2019-05-08 09:43:47 +0200

    finish

--- Praktikum_1_Aufgabe_1_v2/main.cpp
+++ Praktikum_1_Aufgabe_1_v2/main.cpp
@@ -22,17 +22,49 @@ void Printvec(CMyVector test)
 
 void Print(int schritt, CMyVector test, double lamda1, double fvonx, CMyVector grad, double laengegrad, CMyVector xneu, double fvonxneu)
 {
-	std::cout << "Schritt: " << schritt << std::endl;
-	std::cout << "x = ";
+	std::cout << std::endl << "Schritt: " << schritt << std::endl;
+	std::cout << "\tx = ";
 	Printvec(test);
-	std::cout << "lamda = " << lamda1 << std::endl;
-	std::cout << " f(x) = " << fvonx << std::endl;
-	std::cout << "grad f(x) = ";
+	std::cout << "\tlamda = " << lamda1 << std::endl;
+	std::cout << "\tf(x) = " << fvonx << std::endl;
+	std::cout << "\tgrad f(x) = ";
 	Printvec(grad);
-	std::cout << "||grad f(x)|| = " << laengegrad << std::endl << std::endl;
-	std::cout << "x_neu = ";
+	std::cout << "\t||grad f(x)|| = " << laengegrad << std::endl << std::endl;
+	std::cout << "\tx_neu = ";
 	Printvec(xneu);
-	std::cout << "f(x_neu) = " << fvonxneu << std::endl << std::endl;
+	std::cout << "\tf(x_neu) = " << fvonxneu << std::endl << std::endl;
+}
+
+void PrintEnde( CMyVector test, double lamda1, double fvonx, CMyVector grad, double laengegrad, CMyVector xneu, double fvonxneu)
+{
+	std::cout << std::endl << "Ende wegen ||grad f(x)||<1e-5 bei" << std::endl;
+	//std::cout << "Schritt: " << schritt << std::endl;
+	std::cout << "\tx = ";
+	Printvec(test);
+	std::cout << "\tlamda = " << lamda1 << std::endl;
+	std::cout << "\tf(x) = " << fvonx << std::endl;
+	std::cout << "\tgrad f(x) = ";
+	Printvec(grad);
+	std::cout << "\t||grad f(x)|| = " << laengegrad << std::endl << std::endl;
+	//std::cout << "\tx_neu = ";
+	//Printvec(xneu);
+	//std::cout << "\tf(x_neu) = " << fvonxneu << std::endl << std::endl;
+}
+
+void PrintEndeSchritt(CMyVector test, double lamda1, double fvonx, CMyVector grad, double laengegrad, CMyVector xneu, double fvonxneu)
+{
+	std::cout << std::endl << "Ende wegen Schrittanzahl = 25 bei" << std::endl;
+	//std::cout << "Schritt: " << schritt << std::endl;
+	std::cout << "\tx = ";
+	Printvec(test);
+	std::cout << "\tlamda = " << lamda1 << std::endl;
+	std::cout << "\tf(x) = " << fvonx << std::endl;
+	std::cout << "\tgrad f(x) = ";
+	Printvec(grad);
+	std::cout << "\t||grad f(x)|| = " << laengegrad << std::endl << std::endl;
+	//std::cout << "\tx_neu = ";
+	//Printvec(xneu);
+	//std::cout << "\tf(x_neu) = " << fvonxneu << std::endl << std::endl;
 }
 
 CMyVector gradient(CMyVector test, double(*funktion)(CMyVector test))
@@ -42,12 +74,15 @@ CMyVector gradient(CMyVector test, double(*funktion)(CMyVector test))
 	CMyVector rueckgabe(test.getDimension(), v);
 	double h = 1e-8;
 
+	std::cout << std::fixed;
+	std::cout << std::setprecision(6);
+
 	gradient = test;
 	double funkvonF = funktion(test);
 
 	for (int index = 0; index < gradient.getDimension(); index++)
 	{
-		gradient.setValue(gradient.getValue(index) + h, index);	//um die Wackelei zu beginnen 
+		gradient.setValue(gradient.getValue(index) + h, index);	//um die Wackelei zu beginnen  ....überladen von operatoren [] wäre besser
 		rueckgabe.setValue((funktion(gradient) - funkvonF) / h, index);
 		gradient.setValue(test.getValue(index), index);			//um die Wackelei wieder aus der dimension zu nehmen
 	}
@@ -60,7 +95,7 @@ void gradientenverfahren(CMyVector test, double(*funktion)(CMyVector test), doub
 	std::vector<double> v;
 	CMyVector oldvector(test.getDimension(), v);
 	CMyVector newvector(test.getDimension(), v);
-	CMyVector gradient1(test.getDimension, v);
+	CMyVector gradient1(test.getDimension(), v);
 	CMyVector testvector(test.getDimension(), v);
 
 	double lamda = wertfuerlamda;
@@ -69,63 +104,64 @@ void gradientenverfahren(CMyVector test, double(*funktion)(CMyVector test), doub
 
 	//gradient1 = gradient(test, funktion);
 
-	for (int schritt = 0; schritt <= 25; schritt++)
+	for (int schritt = 0; schritt < 25; schritt++)
 	{
 		newvector = oldvector + lamda * gradient(oldvector, funktion);
-	
-		if (newvector.getLength() < 1e-5)
+
+		if (gradient(oldvector, funktion).getLength() < 1e-5)
 		{
+			PrintEnde(oldvector, lamda, funktion(oldvector), gradient(oldvector, funktion), gradient(oldvector, funktion).getLength(), newvector, funktion(newvector));
 			//newvector.
-			break;
+			return;
 		}
 
 		gradient1 = gradient(test, funktion);
 
-		Print(schritt, oldvector, lamda, funktion(oldvector), gradient(oldvector, funktion), newvector.getLength(), newvector, funktion(newvector));
+		Print(schritt, oldvector, lamda, funktion(oldvector), gradient(oldvector, funktion), gradient(oldvector, funktion).getLength(), newvector, funktion(newvector));
 
 		if (funktion(newvector) > funktion(oldvector))
 		{
-			testvector = newvector + 2 * lamda * gradient(newvector, funktion);
-				if (funktion(testvector) > funktion(newvector))
-				{
-					lamda = lamda * 2;
-					oldvector = testvector;
-					std::cout << "Test mit doppelter Schrittweite(lambda = " << lamda << "):" << std::endl;
-					std::cout << "x_Test = ";
-					Printvec(testvector);
-					std::cout << "f(x_test) = " << funktion(testvector) << std::endl;
-					std::cout << "verdoppele Schrittweite!" << std::endl;
-				}
-				else
-				{
-					lamda = lamda;
-					oldvector = newvector;
-					std::cout << "Test mit doppelter Schrittweite (lambda = " << lamda << "):" << std::endl;
-					std::cout << "x_Test = ";
-					Printvec(testvector);
-					std::cout << "f(x_test) = " << funktion(testvector) << std::endl;
-					std::cout << "behalte alte Schrittweite!" << std::endl;
-				}
+			std::cout << "\tTest mit doppelter Schrittweite(lambda = " << lamda*2 << "):" << std::endl;
+			testvector = oldvector/*newvector*/ + 2 * lamda * gradient(oldvector/*newvector*/, funktion);
+			std::cout << "\tx_Test = ";
+			Printvec(testvector);
+			std::cout << "\tf(x_test) = " << funktion(testvector) << std::endl;
+			if (funktion(testvector) > funktion(newvector))
+			{
+				lamda = lamda*2.0;
+				oldvector = testvector;
+				std::cout << "\tverdoppele Schrittweite!" << std::endl;
+			}
+			else
+			{
 				
-				continue;
-		}
+				//lamda = lamda;
+				oldvector = newvector;
+				//std::cout << "Schrittweite beibehlaten (lambda = " << lamda << "):" << std::endl;
+				std::cout << "\tbehalte alte Schrittweite!" << std::endl;
+			}
 
-		if (funktion(newvector) < funktion(oldvector))
+			//continue;
+		}
+		else if (funktion(newvector) <=/*<*/ funktion(oldvector))
 		{
-			while (funktion(testvector) < funktion(oldvector))
+			while (funktion(newvector/*testvector*/) <= funktion(oldvector))
 			{
 				lamda = lamda / 2;
-				testvector = oldvector + lamda * gradient(oldvector, funktion);
+				newvector/*testvector*/ = oldvector + lamda * gradient(oldvector, funktion);
+				std::cout << "\thalbiere Schrittweite (lambda = " << lamda << "):" << std::endl;
+				std::cout << "\tx_neu = ";
+				Printvec(testvector);
+				std::cout << "\tf(x_test) = " << funktion(testvector) << std::endl;
+				//std::cout << "behalte alte Schrittweite!" << std::endl;
+				
 			}
-			std::cout << "halbiere Schrittweite (lambda = "  << lamda << "):" << std::endl;
-			std::cout << "x_neu = ";
-			Printvec(testvector);
-			std::cout << "f(x_test) = " << funktion(testvector) << std::endl;
-			//std::cout << "behalte alte Schrittweite!" << std::endl;
-			oldvector = testvector;
-			continue;
+			oldvector = newvector; /*oldvector=testvector*/
+			//continue;
 		}
+		//oldvector = newvector;
 	}
+	PrintEndeSchritt(oldvector, lamda, funktion(oldvector), gradient(oldvector, funktion), gradient(oldvector, funktion).getLength(), newvector, funktion(newvector));
 }
 /*double CMyVector::getLength()
 {
@@ -180,13 +216,14 @@ int main()
 	vector1.setValue(0.200000, 0);
 	vector1.setValue(-2.100000, 1);
 
+	gradientenverfahren(vector1, ptrf);
+
 	CMyVector vector2(3, v);
 	vector2.setValue(0.00000, 0);
 	vector2.setValue(0.00000, 1);
 	vector2.setValue(0.00000, 2);
 
-	gradientenverfahren(vector1, ptrf);
-	//gradientenverfahren(vector2, ptrg, 0.1);
+	gradientenverfahren(vector2, ptrg, 0.1);
 
 	system("PAUSE");
 
